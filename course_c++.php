2b82aafa733b986c3f40726190a9c8633c03b213
Yerkebulan <erkebulanice@gmail.com>
2020-06-09 22:48:34 +0500

    i

---
+++ course_c++.php
@@ -0,0 +1,341 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/course_style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+    <!-- navbar -->
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal"><a style="text-decoration: none;" href="site.php">CourseSites</a></h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#main.php">Home</a>
+            <a class="p-2 text-dark" href="#">Courses</a>
+            <a class="p-2 text-dark" href="#">About us</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="auth.php">Sign in</a>
+    </div>
+
+    <!-- content -->
+    <div class="main-content-wrapper" >
+        <div class="main-content">
+            <div class="head">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xxs-8 left-col">
+                            <div class="clp-lead">
+                                <div class="clp-component-render">
+                                    <div class="clp-component-render">
+                                        <h1 class="clp-lead__title" style="color: hsl(211, 100%, 50%)">Introduction to C++</h1>
+                                        <div class="clp-lead__headline">Write your first program on programming language C++</div>
+                                    </div>
+                                <div class="clp-lead__element-row">
+                                    <div class="clp-lead__element-item">
+                                        <div class="clp-component-render">
+                                            <div class="registr"><strong>104 students registered</strong></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="clp-lead__element-row">
+                                    <div class="clp-lead__element-item">
+                                        <div class="clp-lead__element-render english_lan">
+                                            <span>
+                                                Mentor: <a href="instagram.com/alpamysdosbol" class="instructor-links__link">Dosbol Alpamys</a>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>    
+                                <div class="clp-lead__element-item">
+                                    <div class="clp-lead__language">
+                                        <div class="clp-lead__locale english_lan">
+                                            <span>
+                                                <svg class="bi bi-chat-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 008 15z"/>
+                                                </svg>
+                                            </span>
+                                            English
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <div class="col-xxs-8 right-col">
+                <img src="c++.jpg" alt="" style="height: 250px; width: 470px" >
+            </div>
+        </div>
+    </div>
+    
+    <!-- container-->
+    <div class="container container--component-margin">
+      
+        <!-- description -->
+        <div class="row row--component-margin">
+            <div class="col-xxs-8 left-col">
+                <div class="clp-component-render">
+                    <div class="description js-user-tracker-click">
+                        <div data-more="+ More" class="js-simple-collapse js-simple-collapse--description" data-purpose="collapse-description-btn">
+                            <div class="js-simple-collapse-inner">
+                                <div class="description__title" style="font-size: 120%;">
+                                    <strong>Description of this course</strong>
+                                </div>
+                                <br>
+                                <div class>
+                                    <p>C++ is a high-level programming language developed by Bjarne Stroustrup at Bell Labs.
+                                    C++ adds object-oriented features to its predecessor, C. C++ is one of the most popular 
+                                    programming language for graphical applications, such as those that run in Windows and 
+                                    Macintosh environments.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+        <!-- planofcourse -->
+        <div class="row row--component-margin">
+            <div class="col-xxs-8 left-col">
+                <div class="clp-component-render">
+                    <div class="curriculum-wrapper">
+                        <span id="curriculum" class="in-page-offset-anchor"></span>
+                        <div class="ud-component--clp--curriculum">
+                        <span style="font-size: 0px;"></span>
+                        <div class="curriculum-header-container">
+                            <div class="header-left">
+                                <div class="curriculum-header-title">
+                                    Plan of course
+                                </div>
+                            </div>
+                            <div class="header-right">
+                                <span>
+                                    <span class="dib">26 lectures</span>
+                                </span>
+                                <span class="curriculum-header-length">
+                                    60 hours
+                                </span>
+                            </div>
+                        </div>
+                
+                <!-- week-1 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Basic syntax, variables, primitive data-types, operators, first C++ program  
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                6 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+
+                <!-- week-2 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Decision making (if-else)  
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                3 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+                <!-- week-3 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Loops  
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                4 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+                <!-- week-4 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Arrays. One dimensional arrays   
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                1 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+                <!-- week-5 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Multi-dimensional arrays   
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                1 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+                <!-- week-6 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Strings  
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                2 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+                <!-- week-7 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Functions and arguments, header files   
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                5 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+                <!-- week-8 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Pointers & references   
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                2 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+                <!-- week-9 -->
+                <div>
+                    <a href="" class="section-container" style="color: black; text-decoration: none;"> 
+                        <div class="section-header-left">
+                            <span class="section-title-wrapper">
+                                <span class="section-title-toggle">
+                                    <span class="section-title-toggle-plus">+</span>
+                                </span>
+                                <span class="section-title-text">
+                                    Pointers as arrays (Dynamic arrays)   
+                                </span>
+                            </span>
+                        </div>
+                        <div class="section-header-right">
+                            <span class="num-items-in-section">
+                                2 lectures
+                            </span>
+                            <span class="section-header-length">50 minut</span>
+                        </div>
+                    </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row row--component-margin">
+        <div class="col-xxs-8 left-col">
+          <div class="clp-component-render">
+            <div class="description js-user-tracker-click">
+              <div data-more="+ More" class="js-simple-collapse js-simple-collapse--description" data-purpose="collapse-description-btn">
+                <div class="js-simple-collapse-inner">
+                  <div class="description__title" style="font-size: 120%;">
+                    <strong>Reviews</strong>
+                  </div>
+                  <br>
+                  <div class>
+                    <form action="">
+                      <textarea name="" id="" cols="80" rows="5"></textarea>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+</body>
+</html>
