67caf3ccef73c74a77523cf1d060f65f1d8c07a4
Yerkebulan <erkebulanice@gmail.com>
2020-06-10 20:25:39 +0500

    i

---
+++ baza.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (id, name, email, password) VALUES
+(1, 'Ulan', 'ukamay@gmail.com', '123456'),
+(2, 'Yerkebulan', 'erkebulanice@gmail.com', '123'),
+(3, 'Aibyn', 'aibyn@gmail.com', '646521'),
+(4, 'Akbota', 'mukhanbet@gmail.com', '20011913'),
+(5, 'Yera', 'ybolat@outlook.com', 'qwerty123'),
+(6, 'Nurs', 'nnnyegenberdiuly@gmail.com', '654321'),
+(7, 'Ulan', 'turamiyeva@mail.ru', 'it1913'),
+(8, 'Aida', 'aida2000@gmail.com', 'gaziz123'),
+(9, 'Azamat', 'a.saiduly@my.com', 'aza24');
